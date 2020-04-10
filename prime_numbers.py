26fe6fceef2fa1a35ff401bdd21919b7fd7129ba
ndahiya012345 <61085543+ndahiya012345@users.noreply.github.com>
2020-04-10 23:39:40 +0530

    Add files via upload

---
+++ prime_numbers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 01:45:27 2020
+
+@author: nitish
+"""
+
+a=10
+x=0
+
+for i in range(a+1):
+    if(i%2==1):
+        print(i)
+        x=x+1
+print("no.of prime nunmbers",x)        
